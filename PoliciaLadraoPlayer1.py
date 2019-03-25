b0c240b173177a49ec4b3097b87c1acc75716a3b
Andre <andrevssouza@gmail.com>
2019-03-24 22:47:06 -0300

    movimentacao atualizada

--- PoliciaLadraoPlayer1.py
+++ PoliciaLadraoPlayer1.py
@@ -1,7 +1,5 @@
 import pygame
 import rpyc
-import sys
-import time
 """
 K_UP                  up arrow
 K_DOWN                down arrow
@@ -20,35 +18,45 @@ pygame.mixer.pre_init(44100, 16, 2, 4096)
 pygame.mixer.__PYGAMEinit__
 pygame.init()
 jogador = 1
+relogio = pygame.time.Clock()
 campo1 = 1200,633
-campo2 = 621, 263
-speed = [621/31, 0]
+campo2 = 620, 261
+dir = [620/31, 0]
+esq = [-620/31, 0]
+up = [0, -261/13]
+down = [0, 261/13]
 screen = pygame.display.set_mode(campo2)
 pygame.display.set_caption("PoliciaLadrao")
 black = 0, 0, 0
 campo2 = pygame.image.load("images\\campo2.png")
 camporect = campo2.get_rect().move([0, 0])
 jog1 = pygame.image.load("images\\pol.png")
-jog1rect = jog1.get_rect().move([1, 1])
+jog1rect = jog1.get_rect().move([620/31, 261/13])
 x = True
+pygame.key.set_repeat(1, 50)
 while x:
+
     for event in pygame.event.get():
+        pygame.key.get_repeat()
         if event.type == pygame.QUIT:
             x = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
-                jog1rect = jog1rect.move(speed)
+                if jog1rect.move(up) > [620/31, 0]:
+                    break
+                jog1rect = jog1rect.move(up)
             if event.key == pygame.K_DOWN:
-                jog1rect = jog1rect.move(speed)
+                jog1rect = jog1rect.move(down)
             if event.key == pygame.K_RIGHT:
-                jog1rect = jog1rect.move(speed)
+                jog1rect = jog1rect.move(dir)
             if event.key == pygame.K_LEFT:
-                jog1rect = jog1rect.move(speed)
+                jog1rect = jog1rect.move(esq)
 
 
     screen.fill(black)
     screen.blit(campo2, camporect)
     screen.blit(jog1, jog1rect)
     pygame.display.flip()
+    relogio.tick(10)
 
 pygame.quit()
