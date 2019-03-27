df2e5ff1fbf3c2ca0d285f03a03c7f3ac5459c9b
Andre <andrevssouza@gmail.com>
2019-03-27 17:44:41 -0300

    criacao da classe parede

--- Objetos.py
+++ Objetos.py
@@ -18,9 +18,17 @@ class moeda(pygame.sprite.Sprite):
         self.image = pygame.image.load("images\\moeda.png")
 
 class policia(pygame.sprite.Sprite):
-    # This class represents a car. It derives from the "Sprite" class in Pygame.
+
     def __init__(self):
         # Call the parent class (Sprite) constructor
         pygame.sprite.Sprite.__init__(self)
         self.rect = pygame.Rect((620/31)*20, (261/13)*5, 16, 16)
         self.image = pygame.image.load("images\\pol.png")
+
+class wall(pygame.sprite.Sprite):
+
+    def __init__(self, x, y):
+        # Call the parent class (Sprite) constructor
+        pygame.sprite.Sprite.__init__(self)
+        self.rect = pygame.Rect(x, y, 16, 16)
+        self.image = pygame.image.load("images\\bloco.png")
